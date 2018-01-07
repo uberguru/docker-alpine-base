f507562ce719c9b753113f7eb8c0e5b048cb2876
mark <mjabryan@gmail.com>
2018-01-07 12:43:55 +0000

    t

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "building"
 
+DOCKER_USER=$1
+DOCKER_PASS=$2
+COMMIT=$3
+TAG=latest
+
+echo "building"
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+export REPO=uberguru/alpine
+#export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker build -f Dockerfile -t "$REPO:$COMMIT" .
+docker tag $REPO:$COMMIT $REPO:$TAG
+docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker push $REPO:$TAG
